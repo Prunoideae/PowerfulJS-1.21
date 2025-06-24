3afce580992964e91dc8b57968715008e4c0f42e
Li Junyu <jlijh@connect.ust.hk>
2025-06-24 23:19:17 +0800

    Allows null to be used in direction checking

--- src/main/java/moe/wolfgirl/powerfuljs/custom/base/validators/MatchDirection.java
+++ src/main/java/moe/wolfgirl/powerfuljs/custom/base/validators/MatchDirection.java
@@ -4,18 +4,29 @@ import moe.wolfgirl.powerfuljs.custom.base.CapabilityValidator;
 import net.minecraft.core.Direction;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
 public class MatchDirection<T> implements CapabilityValidator<T> {
     private final EnumSet<Direction> allowed;
+    private final boolean allowsNull;
 
     public MatchDirection(Direction[] allowed) {
-        this.allowed = EnumSet.copyOf(List.of(allowed));
+        List<Direction> nonNulls = new ArrayList<>();
+        boolean allowsNull = false;
+        for (Direction direction : allowed) {
+            if (direction != null) nonNulls.add(direction);
+            else allowsNull = true;
+        }
+
+        this.allowed = EnumSet.copyOf(nonNulls);
+        this.allowsNull = allowsNull;
     }
 
     @Override
     public boolean test(T info, @Nullable Object context) {
-        return context instanceof Direction direction && allowed.contains(direction);
+        if (allowsNull && context == null) return true;
+        return context instanceof Direction direction && (allowed.contains(direction));
     }
 }
